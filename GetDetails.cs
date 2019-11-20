e2062ec153c03b159c606b4b6d6867bf4b377fee
PoojaChand <cpooja@prokarma.com>
2019-11-20 15:44:12 +0530

    commit

---
+++ GetDetails.cs
@@ -0,0 +1,27 @@
+using System;  
+  
+  namespace PhoneBookSample
+  {
+    
+      class GetDetails : IHandler
+      {
+        public Contact GetData()
+        {
+  
+             Contact contact = new Contact();
+             Console.WriteLine("Enter firstName : ");
+             contact.firstName = Console.ReadLine();
+             Console.WriteLine("Enter lastName : ");
+             contact.lastName = Console.ReadLine();
+             Console.WriteLine("Enter phoneNumber : ");
+             contact.phoneNumber =Convert.ToDouble( Console.ReadLine());
+             Console.WriteLine("Enter emailId : ");
+             contact.emailId = Console.ReadLine();
+             Console.Write("Contact added successfully.");
+             return contact;
+             
+         }
+
+    
+    }
+  }
